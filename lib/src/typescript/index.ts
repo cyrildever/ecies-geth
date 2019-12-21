34839e6d0b25eaa67111cc979c35e1831c9c8aaa
Cyril Dever <cdever@edgewhere.fr>
2019-12-21 17:56:44 +0100

    browser implementation

---
+++ lib/src/typescript/index.ts
@@ -0,0 +1,12 @@
+import { } from './node' //type definitions
+
+// try to use secp256k1, fallback to browser implementation
+try {
+    module.exports = require("./node")
+} catch (e) {
+    if (process.env.ECCRYPTO_NO_FALLBACK) {
+        throw e
+    } else {
+        module.exports = require("./browser")
+    }
+}
